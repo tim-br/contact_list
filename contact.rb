f48d9eff96ff8781eea7e9424cf116a9b60febb5
Timothy <t.williams.im@gmail.com>
2015-07-14 18:41:02 -0400

    modified add_new_user method

--- contact.rb
+++ contact.rb
@@ -2,16 +2,22 @@ require 'pg'
 
 class Contact
 
-  attr_accessor :name, :email
-  attr_reader :num_contacts, :id
+  attr_accessor :name, :email, :phone, :id
+  attr_reader :num_contacts
 
   def initialize(name, email, num_contacts = 0)
     @name = name
     @email = email
+    @phone
+    @id
   end
 
   def save
     conn = Contact.connection
+    conn.prepare('statement1', 'update people set name = $1, email = $2, phone = $3')
+    conn.exec_prepared('statement1', [ self.name, self.email, self.phone])
+    #conn.prepare('statement2', "SELECT lastval('p')"
+
   end
 
   ## Class Methods
@@ -38,8 +44,24 @@ class Contact
 
   def self.add_new_contact(contact)
     conn = Contact.connection
-    conn.prepare('statement1', 'insert into people ( name, email) values ($1, $2)')
-    conn.exec_prepared('statement1', [ contact.name, contact.email])
+    conn.prepare('statement1', 'insert into people ( name, email) values ($1, $2) RETURNING ID')
+    conn.exec_prepared('statement1', [ contact.name, contact.email]) do |results|
+      puts results
+      results.each do |person|
+        contact.id = person['id']
+        puts contact.id
+        # foo =  person.inspect
+        # puts "this is foo {person}"
+        # puts "this is foo at id {person['id']}"
+        # contact.id = foo["id"]
+        # puts contact.id
+        # puts "yo"
+      end
+    end
+    # puts "********************************"
+
+    # puts "this isntiesrtenas #{ret_value.values}"
+
   end
 
   def self.connection
@@ -52,3 +74,26 @@ class Contact
   end
 
 end
+
+contact = Contact.new("tim", "j.ntesr@teinw.ciom")
+Contact.add_new_contact(contact);
+# contact.email = "jeff@gmail.com"
+
+# conn = Contact.connection
+
+# conn.exec('SELECT * FROM people;') do |results|
+#   results.each do |country|
+#     puts country.inspect
+#   end
+# end
+
+# contact.email = "jeff@gmail.com"
+
+
+# contact.save
+
+# conn.exec('SELECT * FROM people;') do |results|
+#   results.each do |country|
+#     puts country.inspect
+#   end
+# end
---
+++ spec/create_contact_test.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require_relative '../contact.rb'
+
+describe Contact do
+
+  before :each do
+    @contact = Footman.new
+  end
+
+end
